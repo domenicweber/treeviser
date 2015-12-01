f38a6ee26fd4c4d8bd9f2be0983525db9c48c067
Domenic Weber <domenic.weber@gmail.com>
2015-12-01 14:38:13 -0700

    Initial commit with Devise edited from :email to :email_address everywhere possible in the external library

---
+++ app/models/account.rb
@@ -0,0 +1,7 @@
+class Account < ActiveRecord::Base
+  alias_attribute :email, :email_address
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
